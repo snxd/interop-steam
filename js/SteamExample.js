068d1ad953427d7dd6d37a6d477c09d36e0c87d5
Nathan Moinvaziri <nathan@solidstatenetworks.com>
2022-08-16 15:44:46 -0700

    Split up Steam example code into different functions.

--- js/SteamExample.js
+++ js/SteamExample.js
@@ -11,32 +11,7 @@ import {steamUser} from "./SteamUser";
 import {steamUserStats} from "./SteamUserStats";
 import {steamUtils} from "./SteamUtils";
 
-function interopLoaded() {
-
-  //notificationCenter.verbose = true;
-  //interop.verbose = true;
-
-  var isSteamRunning = steamAPI.isSteamRunning();
-  console.log(`SteamAPI - IsSteamRunning - ${isSteamRunning}`);
-  if (!isSteamRunning) {
-    console.warn("Steam must be running");
-    return;
-  }
-
-  steamAPI.setAppId(287450); // Rise of Nations app id for testing
-  if (!steamAPI.initialize()) {
-    console.warn("Cannot initialize steam for app");
-    return;
-  }
-
-  var isLoggedOn = steamUser.isLoggedOn();
-  console.log(`SteamUser - IsLoggedOn - ${isLoggedOn}`);
-  if (!isLoggedOn) {
-    return;
-  }
-
-  var appId = steamUtils.getAppID();
-
+const printSteamUtils = (appId) => {
   console.log(`SteamUtils - AppID - ${appId}`);
   console.log(`SteamUtils - IsOverlayEnabled - ${steamUtils.isOverlayEnabled()}`);
   console.log(`SteamUtils - IsSteamRunningInVR - ${steamUtils.isSteamRunningInVR()}`);
@@ -45,7 +20,9 @@ function interopLoaded() {
   console.log(`SteamUtils - ServerRealTime - ${steamUtils.getServerRealTime()}`);
   console.log(`SteamUtils - IPCountry - ${steamUtils.getIPCountry()}`);
   console.log(`SteamUtils - CurrentBatteryPower - ${steamUtils.getCurrentBatteryPower()}`);
+}
 
+const printSteamApps = (appId) => {
   console.log(`SteamApps - IsAppInstalled - ${steamApps.isAppInstalled(appId)}`);
   console.log(`SteamApps - IsSubscribed - ${steamApps.isSubscribed()}`);
   console.log(`SteamApps - IsSubscribedFromWeekend - ${steamApps.isSubscribedFromWeekend()}`);
@@ -57,15 +34,18 @@ function interopLoaded() {
   console.log(`SteamApps - AppBuildId - ${steamApps.getAppBuildId()}`);
   console.log(`SteamApps - GetDLCCount - ${steamApps.getDLCCount()}`);
 
+}
+
+const printSteamAppsDLCs = (appId) => {
   for (var x = 0; x < steamApps.getDLCCount(); x += 1) {
     var dlcData = steamApps.getDLCData(x, appId);
     console.log(`SteamApps - DLC - ${x} - Data ${JSON.stringify(dlcData)}`);
     var isDLCInstalled = steamApps.isDLCInstalled(dlcData.id);
     console.log(`SteamApps - DLC - ${x} - IsInstalled ${isDLCInstalled}`);
   }
+}
 
-  var mySteamId = steamUser.getSteamId();
-
+const printSteamUser = (mySteamId) => {
   console.log(`SteamUser - GetSteamId - ${mySteamId}`);
   console.log(`SteamUser - IsBehindNAT - ${steamUser.isBehindNAT()}`);
   console.log(`SteamUser - IsPhoneVerified - ${steamUser.isPhoneVerified()}`);
@@ -73,39 +53,29 @@ function interopLoaded() {
   console.log(`SteamUser - IsPhoneIdentifying - ${steamUser.isPhoneIdentifying()}`);
   console.log(`SteamUser - IsPhoneRequiringVerification - ${steamUser.isPhoneRequiringVerification()}`);
   console.log(`SteamUser - GetPlayerSteamLevel - ${steamUser.getPlayerSteamLevel()}`);
+}
 
-  var ob0 = notificationCenter.addInstanceObserver("SteamUser", "AuthSessionTicketResponse", steamUser, function (sender, info) {
+const printSteamUserAuthSessionTicket = () => {
+  var observer = notificationCenter.addInstanceObserver("SteamUser", "AuthSessionTicketResponse", steamUser, (sender, info) => {
     console.log(`SteamUser - AuthSessionTicketResponse - ${JSON.stringify(info)}`);
-    ob0.release();
+    observer.release();
   });
+  // Wait for response from SteamUser.AuthSessionTicketResponse notification
   console.log(`SteamUser - AuthSessionTicket - ${steamUser.getAuthSessionTicket()}`);
+}
 
+const printSteamFriends = (mySteamId) => {
   var myPersonaName = steamFriends.getFriendPersonaName(mySteamId);
   var myPersonaState = steamFriends.getFriendPersonaState(mySteamId);
   var myPersonaStateString = steamFriendsPersonaState.nameFromId(myPersonaState);
 
   console.log(`SteamFriends - Current User - ${myPersonaName} (${myPersonaStateString})`);
 
-  function convertBase64ToBinary(base64) {
-    var raw = window.atob(base64);
-    var array = new Uint8ClampedArray(new ArrayBuffer(raw.length));
-    for (var i = 0; i < raw.length; i++) {
-      array[i] = raw.charCodeAt(i);
-    }
-    return array;
-  }
-
-  var canvas = document.getElementById("friendsList");
-  var ctx = null;
-  if (canvas) {
-    canvas.height = 600;
-    ctx = canvas.getContext("2d");
-  }
-
+  // Print friends list
   var friendCount = steamFriends.getFriendCount();
   console.log(`SteamFriends - FriendCount - ${friendCount}`);
 
-  for (var i = 0, y = 0; i < friendCount; i += 1) {
+  for (var i = 0; i < friendCount; i++) {
     var friendSteamId = steamFriends.getFriendByIndex(i);
     var friendPersonaName = steamFriends.getFriendPersonaName(friendSteamId);
     var friendPersonaState = steamFriends.getFriendPersonaState(friendSteamId);
@@ -114,12 +84,44 @@ function interopLoaded() {
     var friendRelationshipString = steamFriendsRelationship.nameFromId(friendRelationship);
 
     console.log(`SteamFriends - Friend ${(i + 1)} - ${friendSteamId} - ${friendPersonaName} (${friendPersonaStateString}) - ${friendRelationshipString}`);
+  }
+}
+
+const activateSteamFriendsOverlayWebPage = (url) => {
+  steamFriends.activateGameOverlayToWebPage(url);
+}
+
+const convertBase64ToBinary = (base64) => {
+  var raw = window.atob(base64);
+  var array = new Uint8ClampedArray(new ArrayBuffer(raw.length));
+  for (var i = 0; i < raw.length; i++) {
+    array[i] = raw.charCodeAt(i);
+  }
+  return array;
+}
+
+const printSteamFriendsAvatars = (canvasId) => {
+  var canvas = document.getElementById(canvasId);
+  var ctx = null;
+  if (canvas) {
+    canvas.height = 600;
+    ctx = canvas.getContext("2d");
+  }
+
+  var friendCount = steamFriends.getFriendCount();
+
+  // Draw friend list to canvas element
+  for (var i = 0, y = 0; i < friendCount; i += 1) {
+    var friendSteamId = steamFriends.getFriendByIndex(i);
+    var friendPersonaName = steamFriends.getFriendPersonaName(friendSteamId);
+    var friendPersonaState = steamFriends.getFriendPersonaState(friendSteamId);
+    var friendPersonaStateString = steamFriendsPersonaState.nameFromId(friendPersonaState);
 
     var friendAvatarIndex = steamFriends.getSmallFriendAvatar(friendSteamId);
     var friendAvatarWidth = steamUtils.getImageWidth(friendAvatarIndex);
     var friendAvatarHeight = steamUtils.getImageHeight(friendAvatarIndex);
 
-    console.log(`SteamUtils - Image ${friendAvatarIndex} - ${friendAvatarWidth}x${friendAvatarHeight}`);
+    console.log(`SteamUtils - Image ${friendPersonaName} - ${friendAvatarIndex} - ${friendAvatarWidth}x${friendAvatarHeight}`);
 
     if (canvas) {
       var friendAvatarRGBA = steamUtils.getImageRGBA(friendAvatarIndex);
@@ -128,24 +130,30 @@ function interopLoaded() {
 
       ctx.putImageData(friendAvatarImageData, 0, y);
       ctx.fillStyle = "white";
-      ctx.fillText(`${friendPersonaName} (${friendPersonaState})`, friendAvatarWidth + 10, y + ((friendAvatarHeight / 2) + 10));
+      ctx.fillText(`${friendPersonaName} (${friendPersonaStateString})`, friendAvatarWidth + 10, y + ((friendAvatarHeight / 2) + 10));
 
       y += friendAvatarHeight;
     }
   }
+}
 
-  var ob1 = notificationCenter.addInstanceObserver("SteamUserStats", "NumberOfCurrentPlayersResponse", steamUserStats, function (sender, info) {
+const printSteamUserStatsNumberOfPlayers = () => {
+  var observer = notificationCenter.addInstanceObserver("SteamUserStats", "NumberOfCurrentPlayersResponse", steamUserStats, (sender, info) => {
     console.log(`SteamUserStats - NumberOfCurrentPlayersResponse - ${JSON.stringify(info)}`);
-    ob1.release();
+    observer.release();
   });
 
+  // Wait for result in SteamUserStats.NumberOfCurrentPlayersResponse notification
+  steamUserStats.getNumberOfCurrentPlayers();
+}
+
+const printSteamUserStatsAchievements = (mySteamId) => {
   var achievementCount = steamUserStats.getNumberOfAchievements();
-  var ob2 = notificationCenter.addInstanceObserver("SteamUserStats", "UserStatsReceivedResponse", steamUserStats, function (sender, info) {
 
+  var observer2 = notificationCenter.addInstanceObserver("SteamUserStats", "UserStatsReceivedResponse", steamUserStats, (sender, info) => {
     console.log(`SteamUserStats - UserStatsReceivedResponse - ${JSON.stringify(info)}`);
-    steamUserStats.requestGlobalAchievementPercentages();
 
-    var ob3 = notificationCenter.addInstanceObserver("SteamUserStats", "GlobalAchievementPercentagesResponse", steamUserStats, function (sender, info) {
+    var observer3 = notificationCenter.addInstanceObserver("SteamUserStats", "GlobalAchievementPercentagesResponse", steamUserStats, (sender, info) => {
       console.log(`SteamUserStats - NumberOfAchievements - ${achievementCount}`);
 
       for (var i = 0; i < achievementCount; i += 1) {
@@ -159,20 +167,61 @@ function interopLoaded() {
         console.log(`SteamUserStats - Achievement ${(i + 1)} - ${achievementName} - "${achievementFriendlyName}" - ${achievementDescription} - ${(achievementHidden === "1") ? "Invisible " : ""}${(achievementAchieved) ? "Achieved ": ""}${achievementAchievedPercent}%`);
       }
 
-      ob3.release();
+      observer3.release();
     });
 
-    ob2.release();
+    // Wait for result in SteamUserStats.GlobalAchievementPercentagesResponse notification
+    steamUserStats.requestGlobalAchievementPercentages();
+
+    observer2.release();
   });
 
-  // Wait for results in notification callbacks
-  steamUserStats.getNumberOfCurrentPlayers();
+  // Wait for result in SteamUserStats.UserStatsReceivedResponse notification
   steamUserStats.requestUserStats(mySteamId);
-
-  steamFriends.activateGameOverlayToWebPage("http://google.com/");
 }
 
-function interopUnloaded() {
+const interopLoaded = () => {
+  // Check if steam is running
+  const isSteamRunning = steamAPI.isSteamRunning();
+  console.log(`SteamAPI - IsSteamRunning - ${isSteamRunning}`);
+  if (!isSteamRunning) {
+    console.warn("Steam must be running");
+    return;
+  }
+
+  steamAPI.setAppId(287450); // Rise of Nations app id for testing
+  if (!steamAPI.initialize()) {
+    console.warn("Cannot initialize steam for app");
+    return;
+  }
+
+  // Check if user is logged in to steam
+  const isLoggedOn = steamUser.isLoggedOn();
+  console.log(`SteamUser - IsLoggedOn - ${isLoggedOn}`);
+  if (!isLoggedOn) {
+    return;
+  }
+
+  const appId = steamUtils.getAppID();
+
+  printSteamUtils(appId);
+  printSteamApps(appId);
+  printSteamAppsDLCs(appId);
+
+  const mySteamId = steamUser.getSteamId();
+  printSteamUser(mySteamId);
+  printSteamUserAuthSessionTicket();
+
+  printSteamFriends(mySteamId);
+  printSteamFriendsAvatars("friendsList");
+
+  printSteamUserStatsNumberOfPlayers();
+  printSteamUserStatsAchievements(mySteamId);
+
+  activateSteamFriendsOverlayWebPage("http://google.com/");
+}
+
+const interopUnloaded = () => {
   // Release instances
 }
 
